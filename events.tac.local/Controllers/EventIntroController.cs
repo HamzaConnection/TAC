4c6522e910157e16baa3b8b97ebc7ec0acec9b34
Hamza Laroussi <hla@dis-play.dk>
2018-02-21 11:48:40 +0100

    At page 65 about to ComponentizingtheWebsite all is working now had problems with publishing sometimes you have to smart publish,

---
+++ events.tac.local/Controllers/EventIntroController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using events.tac.local.Models;
+using Sitecore.Mvc.Presentation;
+using Sitecore.Web.UI.WebControls;
+
+
+namespace events.tac.local.Controllers
+{
+    public class EventIntroController : Controller
+    {
+        // GET: EventIntro
+        public ActionResult Index()
+        {
+            return View(CreateModel());
+        }
+
+        public static EventIntro CreateModel()
+        {
+            var item = RenderingContext.Current.ContextItem;
+            return new EventIntro()
+            {
+                ContentHeading = new HtmlString(FieldRenderer.Render(item, "ContentHeading")),
+                ContentBody = new HtmlString(FieldRenderer.Render(item, "ContentBody")),
+                EventImage = new HtmlString(FieldRenderer.Render(item, "EventImage", "mw=400")),
+                Highlights = new HtmlString(FieldRenderer.Render(item, "Highlights")),
+                ContentIntro = new HtmlString(FieldRenderer.Render(item, "ContentIntro")),
+                StartDate = new HtmlString(FieldRenderer.Render(item, "StartDate")),
+                Duration = new HtmlString(FieldRenderer.Render(item, "Duration")),
+                DifficultyLevel = new HtmlString(FieldRenderer.Render(item, "DifficultyLevel"))
+            };
+        }
+
+    }
+}
---
+++ events.tac.local/Models/EventIntro.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace events.tac.local.Models
+{
+    public class EventIntro
+    {
+        public HtmlString ContentHeading { get; set; }
+        public HtmlString ContentIntro { get; set; }
+        public HtmlString ContentBody { get; set; }
+        public HtmlString EventImage { get; set; }
+        public HtmlString Highlights { get; set; }
+        public HtmlString StartDate { get; set; }
+        public HtmlString Duration { get; set; }
+        public HtmlString DifficultyLevel { get; set; }
+
+    }
+}
