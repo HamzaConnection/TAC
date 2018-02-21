c49d8bdbabd5160bf4d5b99f002059f35761815f
Hamza Laroussi <hla@dis-play.dk>
2018-02-21 08:21:58 +0100

    ignored all .dll All under the ”bin” folder All under the ”obj” folder And the ”vs” folder
    This is a working version getting ready to do inheritance for my _base templates

---
+++ events.tac.local.Tests/Test1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace events.tac.local.Tests
+{
+    public class Test1
+    {
+        [Fact]
+        public void FailedTest()
+        {
+            Assert.True(false);
+        }
+    }
+}
